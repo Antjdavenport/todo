294aa596049f50591a10c6fdbb83f5461f4d5fb6
Anthony Davenport <antjdavenport@gmail.com>
2021-12-14 16:28:45 -0800

    Initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <div className="todo-container">
+        <ul className="todo-list">
+        <li>test</li>
+        </ul>
+    </div>
+    );
+};
+
+export default TodoList;
