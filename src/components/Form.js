d78e98bca7356af8717fe2229e59aa5f3be39b43
Anthony Davenport <antjdavenport@gmail.com>
2021-12-15 22:21:08 -0800

    Linked up the input text so it generates a new todo

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,16 +7,18 @@ const Form = (props) => {
         props.setInputText(e.target.value);
     };
     const submitTodoHandler = (e) => {
-        e.preventDefault(); 
+        e.preventDefault();  
         // prevent browser refreshing when submit is clicked
         props.setTodos([
-            ...props.todos, {text: props.inputText, completed: false, id: Math.random() * 1000},
+            ...props.todos, 
+            {text: props.inputText, completed: false, id: Math.random() * 1000},
         ]);
-        props.setInputText = "";
+        props.setInputText("");
+       
     }
  return (
     <form>
-    <input onChange={inputTextHandler} type="text" className="todo-input" />
+    <input value={props.inputText} onChange={inputTextHandler} type="text" className="todo-input" />
     <button onClick={submitTodoHandler} className="todo-button" type="submit">
       <i className="fas fa-plus-square"></i>
     </button>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Todo from './Todo.js';
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
     return (
         <div className="todo-container">
         <ul className="todo-list">
-        <li>test</li>
+       {todos.map(todo => (
+           <Todo text={todo.text} id={todo.id} />
+       ))}
         </ul>
     </div>
     );
